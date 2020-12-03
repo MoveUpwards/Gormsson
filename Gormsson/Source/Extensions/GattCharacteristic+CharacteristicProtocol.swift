42b8021273c9723aa81640ccbc8c87b202fbabf9
Damien <damien@slide-r.com>
2020-12-03 12:14:45 +0100

    Add check on characteristic + remove the last warning

---
+++ Gormsson/Source/Extensions/GattCharacteristic+CharacteristicProtocol.swift
@@ -0,0 +1,28 @@
+//
+//  GattCharacteristic+CharacteristicProtocol.swift
+//  Gormsson
+//
+//  Created by Mac on 03/12/2020.
+//  Copyright © 2020 Loïc GRIFFIE. All rights reserved.
+//
+
+import Foundation
+
+extension GattCharacteristic {
+    /// Comparaison between GattCharacteristic and CharacteristicProtocol
+    public static func == (lhs: GattCharacteristic, rhs: CharacteristicProtocol) -> Bool {
+        return lhs.characteristic.uuid == rhs.uuid
+    }
+}
+
+extension CharacteristicProtocol {
+    /// Comparaison between two CharacteristicProtocol
+    public static func == (lhs: CharacteristicProtocol, rhs: CharacteristicProtocol) -> Bool {
+        return lhs.uuid == rhs.uuid
+    }
+
+    /// Comparaison between CharacteristicProtocol and GattCharacteristic
+    public static func == (lhs: CharacteristicProtocol, rhs: GattCharacteristic) -> Bool {
+        return lhs.uuid == rhs.characteristic.uuid
+    }
+}
