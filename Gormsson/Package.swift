1183248d1871377bcde23f5d2cd02f93a260a3b2
Damien Noël Dubuisson <damien@mytibtop.com>
2021-01-28 17:54:45 +0100

    Remove all Pod references.

---
+++ Gormsson/Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "Gormsson",
+    platforms: [
+        .iOS(.v10),
+        .macOS(.v10_13)
+    ],
+    products: [
+        .library(
+            name: "Gormsson",
+            targets: ["Gormsson"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/MoveUpwards/Nevanlinna.git", from: "0.3.4")
+    ],
+    targets: [
+        .target(
+            name: "Gormsson",
+            dependencies: ["Nevanlinna"],
+            path: "Source"
+        ),
+    ]
+)
